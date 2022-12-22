b3a5c763468ac551c73e497cbfa592134a0044b3
Даниил <gpakali4@gmail.com>
2022-12-22 10:50:11 +0300

    more test

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ class TestCalculator(unittest.TestCase):
   def setUp(self):
     self.calculator = Calculator()
   def test_add(self):
-    self.assertEqual(self.calculator.add(4,7), 11)
+    self.assertEqual(self.calculator.add(3,3), 6)
+    self.assertEqual(self.calculator.add(3, 10), 13)
   def test_subtract(self):
     self.assertEqual(self.calculator.subtract(10,5), 5)
+    self.assertEqual(self.calculator.subtract(3, 2), 1)
   def test_multiply(self):
-    self.assertEqual(self.calculator.multiply(3,7), 21)
+    self.assertEqual(self.calculator.multiply(2,2), 4)
+    self.assertEqual(self.calculator.multiply(2, 6), 12)
   def test_divide(self):
-    self.assertEqual(self.calculator.divide(10,2), 5)
+    self.assertEqual(self.calculator.divide(150,25), 6)
+    self.assertEqual(self.calculator.divide(100, 10), 10)
 if __name__ == "__main__":
   unittest.main()
